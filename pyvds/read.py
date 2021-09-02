83f1c6710448fef0c5fa3742da10ee00e54e1f52
David Wade <dawad@equinor.com>
2021-09-02 12:12:44 +0200

    This is what happens when your test data has the same number of inlines as crosslines

--- pyvds/read.py
+++ pyvds/read.py
@@ -227,7 +227,7 @@ class VdsReader:
         if not 0 <= index < self.n_ilines * self.n_xlines:
             raise IndexError(self.range_error.format(index, 0, self.tracecount))
 
-        xl_coord, il_coord = index % self.n_ilines, index // self.n_ilines
+        xl_coord, il_coord = index % self.n_xlines, index // self.n_xlines
 
         req = self.access_manager.requestVolumeSubset((0,xl_coord,il_coord), (240,xl_coord+1,il_coord+1),
                                                      channel=self.layout.getChannelIndex('SEGYTraceHeader'),
